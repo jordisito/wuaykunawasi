0f708d3786ee11ab7348d98c54045b73ef05ee6e
jtirado@seek.pe <jordy_unt@live.com>
2017-04-24 23:52:13 -0500

    page complete

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    
+     $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+    
+$(".dropdown-button").dropdown();
+        
+     
+      
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
